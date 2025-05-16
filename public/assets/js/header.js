6d5ca39770fe748478e4b9e8c86e2ce1a8cf2998
Sajid <sajidahamed2001@gmail.com>
2025-05-16 17:02:39 +0530

    header.js

---
+++ public/assets/js/header.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () =>{
+  const header = document.createElement("header");
+  header.id = "header";
+  header.innerHTML = `
+    <a class="logo" href="index.html">Since 1989</a>
+    <nav>
+      <a href="/">Home</a>
+      <a href="Contact.html">Contact Us</a>
+      <a href="elements.html">Elements</a>
+    </nav>
+  `;
+  
+  // Insert at the top of body or in a placeholder
+      document.getElementById("header-placeholder").appendChild(header);
+
+});
+
+
+
+
+    //   <header id="header">
+    //     <a class="logo" href="index.html">Since 1989</a>
+    //     <nav>
+    //       <a href="/">Home</a>
+    //       <a href="Contact.html">Contact Us</a>
+    //       <a href="elements.html">Elements</a>
+    //     </nav>
